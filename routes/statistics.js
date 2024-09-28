3b72301bace7572927b0d53b9b670b12e09e73ae
yoonki1207 <yoonki1207@gmail.com>
2024-09-28 15:50:27 +0900

    Fix:  함수 수정

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import prisma from '../prisma/prismaClient.js';
 import { authenticateToken } from '../utils/authMiddleware.js';
+import { getDatesBetween, getMaxStreak } from '../utils/statisticsUtils.js';
 
 const router = express.Router();
 
+/**
+ * 사용자 루틴 리뷰 통계 조회
+ * 사용자의 루틴 리뷰 데이터를 바탕으로 최근 진행 상황, 최대 연속 기록, 총 진행 일수 등의 통계를 조회합니다.
+ */
 router.get('/', authenticateToken, async (req, res) => {
   try {
+    // 모든 리뷰
     const routineReviews = await prisma.routineReview.findMany({
       where: {
         userId: req.user.id,
@@ -14,7 +20,18 @@ router.get('/', authenticateToken, async (req, res) => {
         createdAt: 'desc',
       },
     });
+    // 만약 수행한 적이 없다면, 전부 0을 응답합니다. 
+    if (routineReviews.length === 0) {
+      response = {
+        maxStreak: 0,
+        recentStreak: 0,
+        totalProcessDays: 0,
+        recentPerformanceRate: 0
+      };
+      res.json(response);
+    }
 
+    // 날짜 중복을 제거합니다.
     const uniqueDates = [
       ...new Set(
         routineReviews.map(
@@ -23,89 +40,59 @@ router.get('/', authenticateToken, async (req, res) => {
       ),
     ];
 
-    const today = new Date().toISOString().split('T')[0];
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    const yesterdayString = yesterday.toISOString().split('T')[0];
-
-    if (uniqueDates.length == 0) {
-      response = {
-        maxStreak: 0,
-        recentStreak: 0,
-        totalProcessDays: 0,
-      };
-      res.json(response);
-    }
-
-    let maxStreak = 1;
-    let currentStreak = 1;
-    let totalProcessDays = 1;
-    let recentStreak = 1;
-    let recentStreakFound = false;
-
-    if (uniqueDates[0] !== today && uniqueDates[0] !== yesterdayString) {
-      recentStreakFound = true;
-      recentStreak = 0;
-    }
-
-    for (let i = 1; i < uniqueDates.length; i++) {
-      const prevDate = new Date(uniqueDates[i - 1]);
-      const currentDate = new Date(uniqueDates[i]);
-      totalProcessDays++;
-
-      const diffDays = Math.floor(
-        (currentDate - prevDate) / (1000 * 60 * 60 * 24)
-      );
-
-      if (diffDays === -1) {
-        currentStreak++;
-        maxStreak = Math.max(maxStreak, currentStreak);
-
-        if (!recentStreakFound) {
-          recentStreak = currentStreak;
-        }
-      } else {
-        maxStreak = Math.max(maxStreak, currentStreak);
-        currentStreak = 1;
-
-        if (!recentStreakFound) {
-          recentStreakFound = true;
-        }
-      }
-    }
-
-    const recentDates = uniqueDates.slice(0, recentStreak);
-
-    let totalValidRoutines = 0;
-
-    for (let date of recentDates) {
-      const routinesOnDate = await prisma.routine.count({
-        where: {
-          userId: req.user.id,
-          OR: [{ deletedAt: null }, { deletedAt: { gte: new Date(date) } }],
-        },
-      });
-
-      totalValidRoutines += routinesOnDate;
-    }
-
-    const totalReviews = await prisma.routineReview.count({
+    const allRoutines = await prisma.routine.findMany({
       where: {
-        userId: req.user.id,
-        createdAt: {
-          gte: new Date(recentDates[recentDates.length - 1]),
-          lt: new Date(
-            new Date(recentDates[0]).setDate(
-              new Date(recentDates[0]).getDate() + 1
-            )
-          ),
-        },
-      },
+        userId: req.user.id
+      }
+    });
+    const allVirtualRoutines = [];
+    
+    allRoutines.forEach(async (routine) => {
+      const virtualRoutines = await prisma.virtualRoutine.findMany({
+        where: {
+          routineId: routine.id
+        }
+      });
+      allVirtualRoutines.push(...virtualRoutines);
     });
 
-    const recentPerformanceRate =
-      (totalReviews / (totalValidRoutines * recentStreak)) * 100;
+    // routine은 updated 순간 직후, 어제까지의 수행 가능 일자를 가져온다. (수행시작시간 <> 업데이트시간)
+    // vroutine은 created 순간 직후, updatedAt 이하의 수행 기능 일자를 가져온다. ()
 
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday.setHours(23, 59, 59, 999);
+
+    const actionDates = [];
+    allRoutines.forEach(routine => {
+      const { updatedAt } = routine;
+      actionDates.push(...getDatesBetween(routine, updatedAt, yesterday));
+    });
+
+    allVirtualRoutines.forEach(routine => {
+      const { createdAt, updatedAt } = routine;
+      actionDates.push(...getDatesBetween(routine, createdAt, updatedAt));
+    });
+
+    const uniqueActionDates = [...new Set(actionDates)].reverse();
+    uniqueDates;
+    const minLength = Math.min(uniqueActionDates.length, uniqueDates.length);
+    let currentStreak = 0;
+    for(let i = 0; i < minLength; i++) {
+      if(uniqueActionDates[i] !== uniqueDates[i]) {
+        break;
+      }
+      currentStreak++;
+    }
+    const recentStreak = currentStreak;
+
+    // 최대 부분 연속 수열
+    const maxStreak = getMaxStreak(uniqueActionDates, uniqueDates);
+    const totalProcessDays = uniqueDates.length;
+    const recentPerformanceRate = Math.min(
+      routineReviews.length*100 / actionDates,
+      100
+    );
     const response = {
       maxStreak: maxStreak,
       recentStreak: recentStreak,
@@ -113,13 +100,16 @@ router.get('/', authenticateToken, async (req, res) => {
       recentPerformanceRate: recentPerformanceRate,
     };
 
-    res.json(response);
-  } catch (error) {
+  } catch(error) {
     console.error('Failed to fetch statistics:', error);
     res.status(500).json({ error: 'Failed to fetch statistics' });
   }
 });
 
+/**
+ * 월별 루틴 리뷰 기록 조회
+ * 사용자가 선택한 월과 연도에 대해 매일의 루틴 수행 상태를 조회합니다. 각 날짜별로 완료된, 실패한, 건너뛴 루틴들을 확인할 수 있습니다.
+ */
 router.get('/calendar', authenticateToken, async (req, res) => {
   const { month, year } = req.query;
 
@@ -213,6 +203,10 @@ router.get('/calendar', authenticateToken, async (req, res) => {
   res.json(response);
 });
 
+/**
+ * 주간 루틴 성과 보고서 조회
+ * 지난 주와 지지난 주의 루틴 성과를 비교 분석하여 보고서를 제공합니다. 주별 성과와 가장 많이 실패한 루틴에 대한 주간 보고서를 포함합니다.
+ */
 router.get('/report', authenticateToken, async (req, res) => {
   const userId = req.user.id;
 
---
+++ utils/statisticsUtils.js
@@ -0,0 +1,73 @@
+/**
+ * 
+ * @param {import("@prisma/client").Routine} routine 
+ * @param {Date} startAt 
+ * @param {Date} endAt 
+ */
+export function getDatesBetween(routine, startAt, endAt) {
+    const actionDates = [];
+    const {repeatDays, startTime} = routine;
+
+    const startAtDateOnly = getOnlyDate(startAt);
+    const endAtDateOnly = getOnlyDate(endAt);
+
+    const startDayOfWeek = startAt.getDay();
+    const startSeconds = getSeconds(startAt);
+
+    if(repeatDays[(startDayOfWeek+6)%7] && startSeconds >= startTime) {
+        actionDates.push(startAtDateOnly);
+    }
+
+    let currentDate = new Date(startAt);
+    currentDate = setDate(currentDate.getDate() + 1);
+    while(getOnlyDate(currentDate) !== endAtDateOnly) {
+        const currentDayOfWeek = currentDate.getDay();
+
+        if(repeatDays[(currentDayOfWeek+6)%7]) {
+            actionDates.push(getOnlyDate(currentDate));
+        }
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return actionDates;
+}
+
+/**
+ * 
+ * @param {*[]} arr 
+ * @param {*[]} brr 
+ * @returns maxStreak
+ */
+export function getMaxStreak(arr, brr) {
+    const n = arr.length;
+    const m = brr.length;
+    let maxStreak = 0;
+
+    // 2D DP 배열 선언 (모두 0으로 초기화)
+    const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            // 배열의 요소가 같으면 이전 단계(dp[i-1][j-1])에서 1을 더해 연속 부분 수열 길이를 갱신
+            if (arr[i - 1] === brr[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                maxStreak = Math.max(maxStreak, dp[i][j]); // 최대 길이 갱신
+            }
+        }
+    }
+
+    return maxStreak;
+}
+
+
+/**
+ * 
+ * @param {Date} date 
+ */
+const getSeconds = (date) => date.getHours() * 3600 + date.getMinutes * 60 + date.getSeconds();
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns 날짜를 반환합니다. 'yyyy-mm-dd'
+ */
+const getOnlyDate = (date) => date.toISOString().split('T')[0];
