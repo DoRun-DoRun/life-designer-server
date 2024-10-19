76b837ac1a81863c06c07f158ee062a89e36a36e
hin6150 <hin6150@gmail.com>
2024-10-19 18:18:17 +0900

    hotfix: prisma import error fixed

--- utils/subRoutineUtils.js
+++ utils/subRoutineUtils.js
@@ -1,27 +1,29 @@
+import prisma from '../prisma/prismaClient.js';
+
 export async function createSubRoutines(routineId, subRoutines) {
-	const maxIndexSubRoutine = await prisma.subRoutine.findFirst({
-		where: { routineId },
-		orderBy: { index: 'desc' },
-		select: { index: true },
-	});
+  const maxIndexSubRoutine = await prisma.subRoutine.findFirst({
+    where: { routineId },
+    orderBy: { index: 'desc' },
+    select: { index: true },
+  });
 
-	const newIndex = maxIndexSubRoutine ? maxIndexSubRoutine.index + 1 : 0;
+  const newIndex = maxIndexSubRoutine ? maxIndexSubRoutine.index + 1 : 0;
 
-	const createdSubRoutines = await Promise.all(
-		subRoutines.map(async (subRoutine, index) => {
-			const { goal, duration, emoji } = subRoutine;
+  const createdSubRoutines = await Promise.all(
+    subRoutines.map(async (subRoutine, index) => {
+      const { goal, duration, emoji } = subRoutine;
 
-			return await prisma.subRoutine.create({
-				data: {
-					routineId,
-					goal,
-					duration,
-					emoji,
-					index: newIndex + index,
-				},
-			});
-		})
-	);
+      return await prisma.subRoutine.create({
+        data: {
+          routineId,
+          goal,
+          duration,
+          emoji,
+          index: newIndex + index,
+        },
+      });
+    })
+  );
 
-	return createdSubRoutines;
+  return createdSubRoutines;
 }
